63b11dae53acc5f9ed131ccf9e05deb8966d26b9
Puneet Dabral <puneetdabral2000@gmail.com>
2022-02-15 00:29:02 +0530

    add category navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
+import Maincomp from './components/home/Maincomp';
+import { Newnav } from './components/newnavbar/Newnav';
 
 function App() {
   return (
     <>
     <Navbar />
+    <Newnav />
+    <Maincomp />
     </>
   );
 }
