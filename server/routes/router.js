30e45922b71a1097c8a99a48f1940bc567637bcb
Puneet Dabral <puneetdabral2000@gmail.com>
2022-04-25 15:29:13 +0530

    single product route to get data

--- server/routes/router.js
+++ server/routes/router.js
@@ -14,6 +14,23 @@ router.get('/getproducts',async(req,res)=>{
         console.log(err.message);
 
     }
+});
+
+//get individual product data api
+router.get('/getproductsone/:id',async(req,res)=>{
+    try{
+      const id = req.params.id;
+      console.log(id)
+    const indivisualdata = await Products.findOne({id:id}); 
+    // console.log(indivisualdata);
+    res.status(201).json(indivisualdata);
+
+    }catch(err){
+        // console.log(err.message);
+        res.status(400).json({message:err.message});
+        
+
+    }
 })
 
 
